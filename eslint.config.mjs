8a13ccd100438f92acd98b56c3a9e98afe89a4a7
Robin <gimenezrobin@protonmail.com>
2026-07-09 14:59:41 +0200

    misc(lint) : add _* variable exclusion in argsIgnorePattern on no-unused-vars rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ export default tseslint.config(
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     },
   },
 
